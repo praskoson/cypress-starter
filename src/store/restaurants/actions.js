65a77f7aff65faff8e3e4ca3d864aa29740baf2f
praskoson <prasinar@gmail.com>
2023-04-13 20:39:44 +0200

    loading error edge case

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,13 +1,20 @@
 export const START_LOADING = 'START_LOADING';
 export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+export const RECORD_LOADING_ERROR = 'RECORD_LOADING_ERROR';
 
 export const loadRestaurants = () => async (dispatch, getState, api) => {
-  dispatch(startLoading());
-  const records = await api.loadRestaurants();
-  dispatch(storeRestaurants(records));
+  try {
+    dispatch(startLoading());
+    const records = await api.loadRestaurants();
+    dispatch(storeRestaurants(records));
+  } catch {
+    dispatch(recordLoadingError());
+  }
 };
 
 const startLoading = () => ({type: START_LOADING});
+
+const recordLoadingError = () => ({type: RECORD_LOADING_ERROR});
 
 const storeRestaurants = records => ({
   type: STORE_RESTAURANTS,
--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,5 +1,9 @@
 import {combineReducers} from 'redux';
-import {START_LOADING, STORE_RESTAURANTS} from './actions';
+import {
+  RECORD_LOADING_ERROR,
+  START_LOADING,
+  STORE_RESTAURANTS,
+} from './actions';
 
 function records(state = [], action) {
   switch (action.type) {
@@ -15,7 +19,19 @@ function loading(state = false, action) {
     case START_LOADING:
       return true;
     case STORE_RESTAURANTS:
+    case RECORD_LOADING_ERROR:
       return false;
+    default:
+      return state;
+  }
+}
+
+function loadError(state = false, action) {
+  switch (action.type) {
+    case START_LOADING:
+      return false;
+    case RECORD_LOADING_ERROR:
+      return true;
     default:
       return state;
   }
@@ -24,4 +40,5 @@ function loading(state = false, action) {
 export default combineReducers({
   records,
   loading,
+  loadError,
 });
