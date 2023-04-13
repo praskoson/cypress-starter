24d0c40a53c3a7f894ca25ebb568b85b25917bf7
praskoson <prasinar@gmail.com>
2023-04-13 18:45:47 +0200

    loading indicator setup tests

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,23 +1,27 @@
 import {useEffect} from 'react';
 import {connect} from 'react-redux';
+import CircularProgress from '@mui/material/CircularProgress';
 import {loadRestaurants} from '../store/restaurants/actions';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 
-export function RestaurantList({loadRestaurants, restaurants}) {
+export function RestaurantList({loadRestaurants, restaurants, loading}) {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
   return (
-    <List>
-      {restaurants.map(restaurant => (
-        <ListItem key={restaurant.id}>
-          <ListItemText>{restaurant.name}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {loading && <CircularProgress />}
+      <List>
+        {restaurants.map(restaurant => (
+          <ListItem key={restaurant.id}>
+            <ListItemText>{restaurant.name}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
 
--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="jest" />
-/// <reference types="@testing-library/jest-dom" />
+// /// <reference types="@testing-library/jest-dom" />
 
 import {render, screen} from '@testing-library/react';
 import {RestaurantList} from './RestaurantList';
@@ -11,15 +11,15 @@ describe('RestaurantList', () => {
   ];
   let loadRestaurants;
 
-  function renderComponent() {
-    loadRestaurants = jest.fn().mockName('loadRestaurants');
+  function renderComponent(propOverrides = {}) {
+    const props = {
+      loadRestaurants: jest.fn().mockName('loadRestaurants'),
+      restaurants,
+      ...propOverrides,
+    };
+    loadRestaurants = props.loadRestaurants;
 
-    render(
-      <RestaurantList
-        loadRestaurants={loadRestaurants}
-        restaurants={restaurants}
-      />,
-    );
+    render(<RestaurantList {...props} />);
   }
   it('loads restaurants on first render', () => {
     renderComponent();
@@ -30,5 +30,15 @@ describe('RestaurantList', () => {
     renderComponent();
     expect(screen.getByText('Sushi Place')).toBeInTheDocument();
     expect(screen.getByText('Pizza Place')).toBeInTheDocument();
+  });
+
+  it('displays the loading indicator while loading', () => {
+    renderComponent({loading: true});
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  it('does not display the loading indicator while not loading', () => {
+    renderComponent({loading: false});
+    expect(screen.queryByRole('progressbar')).not.toBeInTheDocument();
   });
 });
