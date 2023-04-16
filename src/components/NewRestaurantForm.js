2c47b87365a17be250b409581ffc093014f61334
praskoson <prasinar@gmail.com>
2023-04-17 00:44:30 +0200

    integration + initial unit spec for adding restaurant

---
+++ src/components/NewRestaurantForm.js
@@ -0,0 +1,28 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useState} from 'react';
+
+export function NewRestaurantForm({createRestaurant}) {
+  const [name, setName] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    createRestaurant(name);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        value={name}
+        onChange={e => setName(e.target.value)}
+        placeholder="Add Restaurant"
+        fullWidth
+        variant="filled"
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Add
+      </Button>
+    </form>
+  );
+}
+
+export default NewRestaurantForm;
